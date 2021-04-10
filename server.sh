9559517a7940a6cc7a0b6c364039806b553a8a5a
linuxing3 <linuxing3@qq.com>
2021-04-10 14:40:32 +0000

    Commited by root from /root/go/src/github.com/linuxing3/datacenter on 2021-04-10-14:40

--- server.sh
+++ server.sh
@@ -48,4 +48,4 @@ docker run -d --name es \
 	-p 9200:9200 -p 9300:9300 \
 	-e "discovery.type=single-node" \
 	-v $(pwd)/elasticsearch:/usr/share/elasticsearch/data \
-	spencezhou/elasticsearch:7.6.2
+	spencezhou/elasticsearch
