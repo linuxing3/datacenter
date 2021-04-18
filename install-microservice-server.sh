fc90e259080621604a96c038048dfa735e511710
linuxing3 <linuxing3@qq.com>
2021-04-18 19:58:34 +0000

    docker-compose

--- install-microservice-server.sh
+++ install-microservice-server.sh
@@ -17,69 +17,6 @@ yellow() {
     echo -e "\033[33m\033[01m$1\033[0m"
 }
 
-start_adminer() {
-    docker kill datacenter_adminer
-    docker rm datacenter_adminer
-    docker run --name datacenter_adminer \
-        -p 9434:8080 \
-        --link mysql \
-        -e 'MYSQL_PASSWORD=admin' \
-        -d adminer 
-}
-
-##### 启动mysql 服务
-start_mysql() {
-  docker kill mysql
-  docker rm mysql
-  docker run --name mysql -d -p 3306:3306 -e 'MYSQL_ROOT_PASSWORD=admin' -v /home/dev/mysql8:/var/lib/mysql mysql:8.0.21
-}
-
-##### 启动redis 服务
-start_redis() {
-  docker stop redis
-  docker rm redis
-  echo "Start Redis Service..."
-  docker run --name redis -d \
-    --publish 6379:6379 \
-    --env 'REDIS_PASSWORD=admin' \
-    --volume /home/dev/redis:/var/lib/redis \
-    sameersbn/redis:latest
-}
-
-### 启动 etcd 服务
-start_etcd() {
-  rm -rf /home/dev/etcd && mkdir -p /home/dev/etcd && \
-    docker stop etcd && \
-    docker rm etcd || true && \
-    docker run -d \
-    -p 2379:2379 \
-    -p 2380:2380 \
-    --mount type=bind,source=/home/dev/etcd,destination=/etcd-data \
-    --name etcd \
-    quay.io/coreos/etcd \
-    /usr/local/bin/etcd \
-    --name s1 \
-    --data-dir /etcd-data \
-    --listen-client-urls http://0.0.0.0:2379 \
-    --advertise-client-urls http://0.0.0.0:2379 \
-    --listen-peer-urls http://0.0.0.0:2380 \
-    --initial-advertise-peer-urls http://0.0.0.0:2380 \
-    --initial-cluster s1=http://0.0.0.0:2380 \
-    --initial-cluster-token tkn \
-    --initial-cluster-state new
-}
-
-#### 启动elasticsearch 服务
-start_es() {
-  docker stop es
-  docker rm es
-  docker run -d --name es \
-    -p 9200:9200 -p 9300:9300 \
-    -e "discovery.type=single-node" \
-    -v /home/dev/elasticsearch:/usr/share/elasticsearch/data \
-    spencezhou/elasticsearch:7.6.2
-}
-
 install_prometheus() {
     wget https://github.com/prometheus/prometheus/releases/download/v2.26.0/prometheus-2.26.0.linux-amd64.tar.gz
     tar -C /usr/local/ -xvf prometheus-2.26.0.linux-amd64.tar.gz
@@ -102,48 +39,25 @@ function start_menu() {
     green " 系统：debian10+gozero"
     green " ===================================="
     echo
-    green " 1. mysql"
-    green " 2. redis"
-    green " 3. etcd"
-    green " 4. elasticsearch"
-    green " 5. prometheus"
-    green " 6. grafana"
-    green " 7. all"
-    green " 8. stop all container"
+    green " 1. start all servers"
+    green " 2. stop all container"
+    green " 3. prometheus"
+    green " 4. grafana"
     yellow " 0. Exit"
     echo
     read -p "输入数字:" num
     case "$num" in
     1)
-        start_mysql
-        start_adminer
+        docker-compose up -d
         ;;
     2)
-        start_redis
+        docker-compose down
         ;;
     3)
-        start_etcd
-        ;;
-    4)
-        start_es
-        ;;
-    5)
         install_prometheus
         ;;
-    6)
+    4)
         install_grafana
-        ;;
-    7)
-        start_mysql
-        start_redis
-        start_etcd
-        start_es
-        ;;
-    8)
-        docker stop es
-        docker stop etcd
-        docker stop mysql
-        docker stop redis
         ;;
     0)
         exit 1
